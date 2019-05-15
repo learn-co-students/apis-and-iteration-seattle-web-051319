767ec0a20a8312096d426db189948af2f1ef9896
AustinBH <harlowaustin2@gmail.com>
2019-05-14 17:09:55 -0700

    Solved it with Jesse

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,15 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+def welcome
+  puts "Welcome, User"
+end
+
+def get_character_from_user
+  puts "Please enter character name:"
+  gets.chomp
+end
+
 welcome
 character = get_character_from_user
 show_character_movies(character)
